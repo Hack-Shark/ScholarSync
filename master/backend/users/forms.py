9b3b00af27fba457039e5678b34fe98781411a86
Raghuveer22 <posa.mokshith@iitg.ac.in>
2023-06-03 12:40:38 +0530

    email_authentication

--- master/backend/users/forms.py
+++ master/backend/users/forms.py
@@ -4,13 +4,13 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=150)
+    email = forms.EmailField(widget=forms.EmailInput)
     password = forms.CharField(widget=forms.PasswordInput)
     def clean(self):
         cleaned_data = super().clean()
-        username = cleaned_data.get('username')
+        email = cleaned_data.get('email')
         password = cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
+        user = authenticate(email=email, password=password)
         if not user:
             raise forms.ValidationError("Invalid username or password.")
         return cleaned_data
@@ -18,4 +18,4 @@ class LoginForm(forms.Form):
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2')
+        fields = ('username','email','password1', 'password2')
