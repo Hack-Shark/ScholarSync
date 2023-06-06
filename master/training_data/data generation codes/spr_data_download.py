061032f22862196425a859a3737d8c239dcb03e1
Sai Srinivas <mssrinu004@gmail.com>
2023-06-06 18:13:36 +0530

    Update spr_data_download.py

--- master/training_data/data generation codes/spr_data_download.py
+++ master/training_data/data generation codes/spr_data_download.py
@@ -8,6 +8,11 @@ articles_path = 'articles.csv'
 request_failed = 'request_failed.csv'
 request_passed = 'request_passed.csv'
 
+
+def attach(string):
+    return ''.join(string.splitlines())
+
+
 def responser(url):
     try:
         response = requests.get(url)
@@ -40,80 +45,95 @@ def responser(url):
             f.write(f'{url}\n')
         return None
 
-d_links = []
-def extract_articles(url, alp,j):
+
+def getdata(url):
+    r = responser(url)
+    if r is not None:
+        soup = BeautifulSoup(r.content, "html.parser")
+        return soup
+    else:
+        return None
+
+
+def getnextpage(soup):
+    page = soup.find("form", attrs={'class': 'pagination'})
+    if not page:
+        return
+    else:
+        if not page.find("img", attrs={'alt': 'next disabled'}):
+            url = "https://link.springer.com" + \
+                (page.find("a", attrs={'class': 'next'})).get('href')
+            return url
+        else:
+            return None
+
+
+def downloadlink_extract(url, alp, j):
     webpage = responser(url)
     if webpage is None:
         return
     soup = BeautifulSoup(webpage.content, "html.parser")
     links = soup.find_all("a", attrs={'class': 'c-atoz-list__link'})
-    for x in links:
-        link_href = x.get('href')
-        print(link_href)
-        link=link_href.rsplit('/', 1)
-        download_href = "https://link.springer.com/search/csv?query=&search-within=Journal&package=openaccessarticles&facet-journal-id=" +link[-1]
-        d_links.append(download_href)
-    print(len(d_links))
-    download_csv(d_links,alp,j)
+
+    for i, x in enumerate(links):
+        l = x.get('href')
+        articles = responser(l)
+        if articles is not None:
+            soup = BeautifulSoup(articles.content, "html.parser")
+            title = soup.find('title').text
+            open_articles = soup.find_all(
+                "a", attrs={'data-track-action': 'click open access articles'})
+            if open_articles:
+                link = open_articles[0].get('href')
+                # print(link)
+                csv_link = link.rsplit('?', 1)
+                # print(csv_link)
+                download_href = f"{csv_link[0]}/csv?{csv_link[1]}"
+                # print(download_href)
+                csv_data(download_href, alp, j, i, title)
 
 
-# URL of the download link
-# https://link.springer.com/journal/10530
-# download_link = 'https://link.springer.com/search/csv?query=&search-within=Journal&package=openaccessarticles&facet-journal-id=10530'
-def download_csv(download_links,alp,j):
+def csv_data(download_links, alp, j, i, title):
     if download_links is None:
         return
-    for i,download_link in enumerate(download_links):
-        # if i<=65: continue
-        # Send an HTTP GET request to the download link
-        response = responser(download_link)
-        # response = requests.get(download_link)
+    response = responser(download_links)
 
-        # Check if the request was successful (status code 200)
-        if response is not None:
-            # Extract filename from the download link
-            filename = f"{alp}_{j}.csv"
+    # Check if the request was successful
+    if response is not None:
+        # Extract filename from the download link
+        filename = f"{alp}_{j}.csv"
 
-            # Decode the response content as text
-            content = response.content.decode('utf-8')
+        # Decode the response content as text
+        content = response.content.decode('utf-8')
 
-            # Split the content into lines
-            lines = content.splitlines()
+        # Split the content into lines
+        lines = content.splitlines()
 
-            # Remove the first line if it exists and it's not the first file
-            if i > 0 and len(lines) > 0:
-                lines = lines[1:]
+        # Remove the first line if it exists
+        if len(lines) > 0:
+            lines = lines[1:]
 
-            # Join the lines back into a single string
-            content = '\n'.join(lines)
+        # Join the lines back into a single string
+        content = '\n'.join(lines)
 
-            # Save the downloaded file content to the output file
-            with open(filename, 'a', newline='', encoding='utf-8') as file:
-                file.write(content)
+        # Save the downloaded file content to the output file
+        with open(filename, 'a', newline='', encoding='utf-8') as file:
+            file.write(content)
 
-            print(f"File downloaded successfully, i={i}")
-        else:
-            print(f"Failed to download the file. {download_link.split('-')[-1]}")
-        # exit()
+        print(f"File downloaded successfully, link_no:{i+1}.{title}")
+    else:
+        print(f"Failed to download the file. {download_links.split('-')[-1]}")
 
 
 def iter():
-    alpha='cdefghijklmnopqrstuvwxyz'
-    # alpha = 'b'
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
     for i in range(len(alpha)):
-        for j in range(1,5):
+        for j in range(1, 5):
             alp = alpha[i]
             url = f"https://link.springer.com/journals/{alp}/{j}"
-            extract_articles(url, alp,j)
+            downloadlink_extract(url, alp, j)
             print(i, j)
             print(time.time()-start_time)
-
-    # for i in range(len(alpha)):
-    #     alp = alpha[i]
-    #     url = f"https://link.springer.com/journals/{alp}/2"
-    #     extract_articles(url, alp,2)
-    #     print(i, 2)
-    #     print(time.time()-start_time)
 
 
 iter()
