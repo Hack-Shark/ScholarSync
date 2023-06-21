27df3e0bcdb191ad3b2df5d93663f2b358a9fb1f
Sai-004 <94926227+Sai-004@users.noreply.github.com>
2023-06-22 02:20:16 +0530

    Sending Emails on interests

--- master/backend/base/utils.py
+++ master/backend/base/utils.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
 import re
 import nltk
 from nltk.corpus import stopwords
@@ -5,11 +7,68 @@ from nltk.corpus import stopwords
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 
+
 def preprocessing(text):
-    clean_text=''
+    clean_text = ''
     words = re.findall(r'\b\w+\b', text.lower())
     words = text.lower().split()
     for word in words:
-        if( re.match(r'^[a-zA-Z]+$', word) and  word not in stop_words and len(word) >1 and word[1] != '.'):
-            clean_text=clean_text+' '+word
+        if (re.match(r'^[a-zA-Z]+$', word) and word not in stop_words and len(word) > 1 and word[1] != '.'):
+            clean_text = clean_text+' '+word
     return clean_text
+
+
+def article_data(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    title = soup.find("h1", attrs={'class': 'c-article-title'}).text
+    # Check if the abstract element exists
+    abstract_element = soup.find("div", attrs={'id': 'Abs1-content'})
+    abstract = abstract_element.find("p").text if abstract_element else ""
+    authors = []
+    author_tags = soup.find_all('a', attrs={'data-test': 'author-name'})
+    for author_tag in author_tags:
+        author_name = author_tag.text.strip()
+        authors.append(author_name)
+    
+    published = soup.find_all("li", attrs={'class': 'c-bibliographic-information__list-item'})
+    for x in published:
+        if x.find("p").text.startswith("Published"):
+            published_date = x.find("p").find("time").text
+    download_link ="https://link.springer.com"+soup.find('a', attrs={'class': 'c-pdf-download__link'}).get('href')
+
+    return ({'title': title, 'abstract': abstract, 'authors': authors, 'published_date': published_date, 'url': url, 'download_link': download_link})
+
+
+from .models import Article, UserArticle
+
+def process_articles(links,user):
+    processed_links = []
+
+    for link in links:
+        title, url, article_id, journal_id = link
+
+        # Check if the article already exists in the Article model
+        try:
+            article = Article.objects.get(article_id=article_id, journal_id=journal_id)
+        except Article.DoesNotExist:
+            # Article doesn't exist, create a new entry
+            print(url)
+            article_info = article_data(url)
+            if article_info['abstract'] == "" or None:
+                continue  # Skip processing if the abstract is null
+            article = Article.objects.create(article_id=article_id, journal_id=journal_id, article_abstract=article_info['abstract'])
+
+        # Check if the article is associated with the user in the UserArticle model
+        user_article_exists = UserArticle.objects.filter(user=user, article=article).exists()
+
+        if not user_article_exists:
+            # Article is not associated with the user, create a new entry in UserArticle model
+            user_article = UserArticle.objects.create(user=user, article=article)
+            processed_links.append(url)
+
+        if len(processed_links) == 5:
+            break
+
+    return processed_links
--- master/backend/users/scheduler.py
+++ master/backend/users/scheduler.py
@@ -1,32 +1,66 @@
 import time
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from .models import EmailTime
-from datetime import timedelta
 from datetime import datetime
-def send_email(email, subject, message):
-    send_mail(subject, message, settings.EMAIL_HOST_USER, [email])
+from django.utils.html import strip_tags
+from django.template.loader import render_to_string
+from base.cache_builder import get_links
+from base.models import CombinedText
+from base.utils import process_articles,article_data
+
+
+def send_email(email, subject,link_data):
+    # Render the HTML template
+    html_content = render_to_string('email.html', {'link_data': link_data})
+    # Strip HTML tags for the text version of the email
+    text_content = strip_tags(html_content)
+
+    msg = EmailMultiAlternatives(
+        subject, text_content, settings.EMAIL_HOST_USER, [email])
+    # Attach the HTML content to the email
+    msg.attach_alternative(html_content, "text/html")
+
+    msg.send()
+
 
 def schedule_emails():
-    email_times = EmailTime.objects.all()
-    print("hi")
+    print('sending mails')
     while True:
-        current_time = time.strftime('%H:%M:%S')  
+        current_time = datetime.now().time()
+        current_hour = current_time.hour
+        # current_minute = current_time.minute
+
+        email_times = EmailTime.objects.all()
 
         for email_time in email_times:
-            time_to_send = email_time.time.strftime('%H:%M:%S')
-            frequency = email_time.freq.time
             email = email_time.email
-            subject = "Your email subject"
-            message = "Your email message"
+            email_hour = email_time.time.hour
+            # email_minute = email_time.time.minute
 
-            if current_time == time_to_send:
-                send_email(email=email, subject=subject, message=message)
+            if current_hour == email_hour:
+                user=email_time.user
+                mailing_time = time.time()
+                text=CombinedText.objects.get(user=user).combined_text
+                related_links=get_links(text)
+                print(f'time to get links = {time.time()-mailing_time}')
+                top5_links=process_articles(related_links,user)
+                print(f'time to process links = {time.time()-mailing_time}')                
+                link_data=[]
+                for link in top5_links:
+                    link_data.append(article_data(link))
+                print(link_data)
+                print(f'time to get link_data = {time.time()-mailing_time}')
+                print(f'got links in {time.time()-mailing_time}')
+                subject = "Your weekly feed from Scholar Sync"
+                message = "Your weekly feed from Scholar Sync"
 
-                # Update the time to send for the next email
-                new_time = (datetime.combine(datetime.today(), email_time.time) + frequency).time()
-                email_time.time = new_time
-                email_time.save()
+                send_email(email=email, subject=subject,link_data=link_data)
+                print(f'sent mail to {email} at {current_time} in {time.time()-mailing_time}')
+            # Update the time to send for the next email
+            new_time = (datetime.combine(datetime.today(), email_time.time) + email_time.freq.time).time()
+            email_time.time = new_time
+            email_time.save()
 
-        time.sleep(1)  
+        time.sleep(60)  # Sleep for 1 minute
 
