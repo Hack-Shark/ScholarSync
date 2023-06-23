8db63f136b918c442a468daa3f55915e41c8b5c8
Raghuveer22 <posa.mokshith@iitg.ac.in>
2023-06-23 14:27:01 +0530

    ready to deploy deploy

---
+++ master/backend/Procfile
@@ -0,0 +1,1 @@
+web: backend.wsgi --log-file -
--- master/backend/backend/settings.py
+++ master/backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG',cast=bool,default=True)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,7 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static/')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
