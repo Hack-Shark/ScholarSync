b8395ec24d2f7abb92ea9a6239d15ea869f6dfc5
Sai-004 <94926227+Sai-004@users.noreply.github.com>
2023-06-24 12:03:35 +0530

    Created Published Year filter

--- master/backend/base/forms.py
+++ master/backend/base/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from .models import Preference
+from datetime import date
 
 class PrefAddForm(forms.ModelForm):
+    YEARS_CHOICES =[(None, 'Enter the Year')]+[(year, year) for year in range(1900, date.today().year + 1)]
+    
+    after = forms.ChoiceField(choices=YEARS_CHOICES, widget=forms.Select(attrs={'class': 'form-control', 'id': 'after'}))
+
     class Meta:
-        model =  Preference
-        fields = ['text','after']
+        model = Preference
+        fields = ['text', 'after']
         widgets = {
-            'text' : forms.TextInput(attrs={'class':'form-control','id':'text'}),
-            'after' : forms.DateInput(attrs={'type': 'date', 'class': 'form-control', 'id':'after'}),
+            'text': forms.TextInput(attrs={'class': 'form-control', 'id': 'text'}),
         }
